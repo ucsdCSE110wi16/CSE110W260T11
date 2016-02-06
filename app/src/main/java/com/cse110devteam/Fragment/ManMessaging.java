77f7de2984913b686a9e8bcea9028756e6aa062b
anthonyaltieri <Anthonyraltieri@gmail.com>
2016-02-06 13:41:03 -0800

    Set up the basic outline of the manager chat for the main job-wide chat. The RecyclerView still needs to be implemented correctly and we need to make our own custom message adapter. I also made a basic mock-up for a chat message that we should use as a template for the initial coding of messages.

--- app/src/main/java/com/cse110devteam/Fragment/ManMessaging.java
+++ app/src/main/java/com/cse110devteam/Fragment/ManMessaging.java
@@ -1,21 +1,46 @@
 package com.cse110devteam.Fragment;
 
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListAdapter;
 
 import com.cse110devteam.R;
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by anthonyaltieri on 1/16/16.
  */
 public class ManMessaging extends android.support.v4.app.Fragment {
+    private EditText inMsg;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSocket.on("new message", onNewMessage);
+        mSocket.connect();
+        inMsg = (EditText) getActivity().findViewById(R.id.inputMessage);
+        mRecyclerView = (RecyclerView) getActivity().findViewById(R.id.recyclerview);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -27,4 +52,49 @@ public class ManMessaging extends android.support.v4.app.Fragment {
     public void onPause() {
         super.onPause();
     }
+
+
+    // Socket.io setup
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://chat.socket.io");
+        } catch (URISyntaxException e){}
+    }
+
+    // Socket.io Emission/Listening
+    private void attemptSend(){
+        String message = inMsg.getText().toString().trim();
+        if (TextUtils.isEmpty(message)){
+            return;
+        }
+        inMsg.setText("");
+        mSocket.emit("new message", message);
+    }
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener(){
+        @Override
+        public void call(final Object... args){
+            getActivity().runOnUiThread(new Runnable(){
+                @Override
+                public void run(){
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try{
+                        username = data.getString("username");
+                        message = data.getString("message");
+                    } catch (JSONException e){
+                        return;
+                    }
+
+                    HashMap<String, String> msgMap = new HashMap<String, String>(2);
+                    msgMap.put("msg", message);
+                    msgMap.put("username", username);
+                    JSONObject msgObj = new JSONObject(msgMap);
+                }
+            });
+        }
+    };
 }
+
