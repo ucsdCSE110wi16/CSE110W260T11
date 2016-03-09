a5fa8e3f838c405d1665dc17bafffd2ff6138f59
Vidur Butalia <vidurbutalia@gmail.com>
2016-03-08 22:38:01 -0800

    Finally got the Manager and Employee schedule up.

--- app/src/main/java/com/cse110devteam/Fragment/EmpSchedule.java
+++ app/src/main/java/com/cse110devteam/Fragment/EmpSchedule.java
@@ -1,37 +1,41 @@
 package com.cse110devteam.Fragment;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.R.id;
-import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.ListView;
+
 
 import com.cse110devteam.R;
-import com.parse.Parse;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.roomorama.caldroid.CaldroidFragment;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
 
 /**
  * Created by anthonyaltieri on 1/22/16.
  */
 public class EmpSchedule extends android.support.v4.app.Fragment {
+    public ArrayList<String> initShifts;
+    public ArrayAdapter<String> shiftAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,44 +60,58 @@ public class EmpSchedule extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.employee_schedule, container, false);
-
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
-        Bundle args = new Bundle();
-        Calendar cal = Calendar.getInstance();
-        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-        caldroidFragment.setArguments(args);
-        int[] limit = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        JSONArray schedArray = ParseUser.getCurrentUser().getJSONArray("boolSched");
-        if(schedArray != null) {
-            HashMap<Date, Integer> schedHM = new HashMap<>();
-            Calendar workCalendar = Calendar.getInstance();
-            for (int i = 0; i < 12; i++) {
-                JSONArray thisMonth = null;
-                try {
-                    thisMonth = schedArray.getJSONArray(i);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                for (int j = 0; j < limit[i]; j++) {
-                    boolean working = thisMonth.optBoolean(j);
-                    if (working) {
-                        workCalendar.set(2016, i, j);
-                        Date workingDay = workCalendar.getTime();
-                        schedHM.put(workingDay, R.color.caldroid_light_red);
-                    }
-                }
+        final View rootView = inflater.inflate(R.layout.employee_schedule, container, false);
+        CalendarView calendarView = (CalendarView) rootView.findViewById(R.id.employee_calendar);
+        ListView listView = (ListView) rootView.findViewById(R.id.employee_shift_list);
+        Calendar today = Calendar.getInstance();
+        initShifts = getShifts(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
+        shiftAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),R.layout.listitem, R.id.textview, initShifts);
+        listView.setAdapter(shiftAdapter);
+        shiftAdapter.setNotifyOnChange(true);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                initShifts.clear();
+                initShifts.addAll(getShifts(year, month, dayOfMonth));
+                shiftAdapter.notifyDataSetChanged();
             }
-            caldroidFragment.setTextColorForDates(schedHM);
-            caldroidFragment.refreshView();
-        }
-        FragmentManager fragManager = myContext.getSupportFragmentManager();
-
-        android.support.v4.app.FragmentTransaction t = fragManager.beginTransaction();
-        t.replace(R.id.fragment_place, caldroidFragment);
-        t.commit();
+        });
+        shiftAdapter.notifyDataSetChanged();
         return rootView;
     }
 
+    public ArrayList<String> getShifts(int year, int month, int day) {
+        final ArrayList<String> shifts = new ArrayList<>();
+        List<ParseObject> ParseShifts = null;
+        Calendar startCalendar = Calendar.getInstance();
+        Calendar endCalendar = Calendar.getInstance();
+        startCalendar.set(year, month, day, 0, 0);
+        endCalendar.set(year, month, day + 1, 0, 0);
+        Date startDate = startCalendar.getTime();
+        Date endDate = endCalendar.getTime();
+
+        final SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm", Locale.US);
+
+        ParseQuery<ParseObject> shiftQuery = ParseQuery.getQuery("Shift");
+        shiftQuery.whereEqualTo("business", ParseUser.getCurrentUser().get("business"));
+        shiftQuery.whereGreaterThan("start", startDate);
+        shiftQuery.whereLessThan("end", endDate);
+        shiftQuery.include("employee");
+        try {
+            ParseShifts = shiftQuery.find();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if(ParseShifts != null){
+            for (ParseObject shift : ParseShifts) {
+                String shiftString;
+                Date beginning = shift.getDate("start");
+                Date end = shift.getDate("end");
+                ParseObject employee = shift.getParseObject("employee");
+                shiftString = dateFormatter.format(beginning) + " - " + dateFormatter.format(end) + ": " + employee.getString("firstname") + " " + employee.getString("lastname");
+                shifts.add(shiftString);
+            }
+        }
+        return shifts;
+    }
 }
